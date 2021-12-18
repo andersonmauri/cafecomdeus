457d6a6d6b0c416799c8da2c26c0c06cae8b451a
Anderson Almeida <maurianderson@gmail.com>
2021-12-18 16:47:12 -0300

    Atualização de Layout

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //animação menu
 let list = document.querySelectorAll('.list');
-function setActiveCLass (){
-    list.forEach((item)=>
-    item.classList.remove('active'));
+function setActiveCLass() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
     this.classList.add('active');
 }
 
-list.forEach((item)=>
+list.forEach((item) =>
     item.addEventListener('mouseover', setActiveCLass)
 )
 
@@ -14,17 +14,17 @@ list.forEach((item)=>
 // slideshow
 
 let totalSlides = document.querySelectorAll('.slider-item').length;
-let currentSlide =0;
+let currentSlide = 0;
 
-document.querySelector('.slides').style.width = 
+document.querySelector('.slides').style.width =
     `calc(100vw * ${totalSlides})`;
-document.querySelector('.slider-controls').style.height = 
-    `${document.querySelector( '.slider').clientHeight}px`;
+document.querySelector('.slider-controls').style.height =
+    `${document.querySelector('.slider').clientHeight}px`;
 
 
-function goPrev(){
+function goPrev() {
     currentSlide--;
-    if(currentSlide < 0){
+    if (currentSlide < 0) {
         currentSlide = totalSlides - 1;
 
     }
@@ -32,21 +32,51 @@ function goPrev(){
 
 }
 
-function goNext(){
+function goNext() {
     currentSlide++;
-    if(currentSlide > (totalSlides - 1)) {
+    if (currentSlide > (totalSlides - 1)) {
         currentSlide = 0;
 
     }
     updateMargin();
 }
 
-function updateMargin(){
+function updateMargin() {
     let sliderItemWidth = document.querySelector('.slider-item').clientWidth;
     let newMargin = (currentSlide * sliderItemWidth);
-    document.querySelector('.slides').style.marginLeft = 
-    `-${newMargin}px`;
+    document.querySelector('.slides').style.marginLeft =
+        `-${newMargin}px`;
 }
 
 
-//setInterval (goNext, 6000);
+setInterval(goNext, 6000);
+
+
+// gallery item filter
+
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+
+        for (let k = 0; k < items.length; k++) {
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")) {
+                items[k].style.display = "block";
+            }
+            if (target == "all") {
+                items[k].style.display = "block";
+            }
+        }
+
+    })
+}
+
+
+
